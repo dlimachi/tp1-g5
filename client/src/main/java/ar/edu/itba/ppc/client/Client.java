a60e5deab2db9607bccd90d8aa0e7cde6b5ddc08
dlimachi <dlimachi@itba.edu.ar>
2024-09-16 23:16:02 -0300

    Make a request in client to create a room and doctor

--- client/src/main/java/ar/edu/itba/ppc/client/Client.java
+++ client/src/main/java/ar/edu/itba/ppc/client/Client.java
@@ -1,11 +1,16 @@
 package ar.edu.itba.ppc.client;
 
+import ar.edu.itba.tp1g5.AddDoctorRequest;
+import ar.edu.itba.tp1g5.AddDoctorResponse;
+import ar.edu.itba.tp1g5.AddRoomResponse;
+import com.google.protobuf.Empty;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
+import ar.edu.itba.tp1g5.EmergencyAdminServiceGrpc;
 
 public class Client {
     private static Logger logger = LoggerFactory.getLogger(Client.class);
@@ -18,8 +23,24 @@ public class Client {
                 .build();
 
         try {
+            AddDoctorRequest request = AddDoctorRequest.newBuilder()
+                    .setDoctorName("Dr. House")
+                    .setLevel(1)
+                    .build();
 
-        } finally {
+            EmergencyAdminServiceGrpc.emergencyAdminServiceBlockingStub blockingStub =
+                    EmergencyAdminServiceGrpc.newBlockingStub(channel);
+            AddRoomResponse reply = blockingStub.addRoom(Empty.newBuilder().build());
+            AddDoctorResponse reply2 = blockingStub.addDoctor(request);
+            System.out.println(reply.getRoom());
+            System.out.println(reply2.getDoctorName());
+            System.out.println(reply2.getLevel());
+
+        }
+        catch (Exception e) {
+            logger.error("Error: " + e.getMessage());
+            }
+        finally {
             channel.shutdown().awaitTermination(10, TimeUnit.SECONDS);
         }
     }
