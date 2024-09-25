be2306ccc213ec1706d7ddc127cb724615027c48
dlimachi <dlimachi@itba.edu.ar>
2024-09-25 13:03:47 -0300

    modified care client

--- client/src/main/java/ar/edu/itba/ppc/client/EmergencyCareClient.java
+++ client/src/main/java/ar/edu/itba/ppc/client/EmergencyCareClient.java
@@ -1,15 +1,19 @@
 package ar.edu.itba.ppc.client;
 
 import ar.edu.itba.ppc.client.utilsConsole.ClientArgs;
+import ar.edu.itba.ppc.client.utilsConsole.ClientCallback;
 import ar.edu.itba.tp1g5.EmergencyCareRequest;
 import ar.edu.itba.tp1g5.EmergencyCareResponse;
 import ar.edu.itba.tp1g5.EmergencyCareServiceGrpc;
+import com.google.protobuf.Empty;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -20,9 +24,9 @@ public class EmergencyCareClient {
     private static CountDownLatch latch;
 
     public static void main(String[] args) throws InterruptedException {
-        logger.info("tp1-g5 Client Starting ...");
+        logger.info("tp1-g5 Emergency Care Client Starting ...");
         logger.info("grpc-com-patterns Client Starting ...");
-        Map<String,String> argMap = parseArgs(args);
+        Map<String, String> argMap = parseArgs(args);
         final String serverAddress = argMap.get(ClientArgs.SERVER_ADDRESS.getValue());
         final String action = argMap.get(ClientArgs.ACTION.getValue());
 
@@ -30,54 +34,54 @@ public class EmergencyCareClient {
                 .usePlaintext()
                 .build();
 
-        EmergencyCareServiceGrpc.EmergencyCareServiceBlockingStub blockingStub =
-                EmergencyCareServiceGrpc.newBlockingStub(channel);
-
-        switch (action) {
-            case "carePatient" -> {
-                final String room = argMap.get(ClientArgs.ROOM.getValue());
-                latch = new CountDownLatch(1);
-                EmergencyCareRequest request = EmergencyCareRequest.newBuilder()
-                        .setRoom(Integer.parseInt(room))
-                        .build();
-                EmergencyCareResponse response = blockingStub.startEmergencyCare(request);
-                System.out.println(String.format("Patient %s (%d) and Doctor %s (%d) are now in Room #$d",
-                        response.getPatientName(), response.getPatientLevel(), response.getDoctorName(), response.getDoctorLevel(), response.getRoom()));
-            }
-            case "dischargePatient" -> {
-                final String room = argMap.get(ClientArgs.ROOM.getValue());
-                final String doctorName = argMap.get(ClientArgs.DOCTOR.getValue());
-                final String patientName = argMap.get(ClientArgs.PATIENT.getValue());
-                latch = new CountDownLatch(1);
-                EmergencyCareRequest request = EmergencyCareRequest.newBuilder()
-                        .setRoom(Integer.parseInt(room))
-                        .setDoctorName(doctorName)
-                        .setPatientName(patientName)
-                        .build();
-                EmergencyCareResponse response = blockingStub.endEmergencyCare(request);
-                System.out.println(String.format("Patient %d (%d) has been discharged from Doctor %s (%d) and the Room #$d is now %d",
-                        response.getPatientName(), response.getPatientLevel(), response.getDoctorName(), response.getDoctorLevel(), response.getRoom(), response.getRoomStatus()));
-            }
-        }
-
         try {
-            /*EmergencyCareRequest requested = EmergencyCareRequest.newBuilder()
-                    .setRoom(1)
-                    .build();
+            EmergencyCareServiceGrpc.EmergencyCareServiceBlockingStub blockingStub =
+                    EmergencyCareServiceGrpc.newBlockingStub(channel);
 
-            EmergencyCareResponse reply = blockingStub.startEmergencyCare(requested);
-            System.out.println(String.format("Start emergency care in %s with %s", reply.getRoom(), reply.getDoctorName()));
-
-             */
-            logger.info("Waiting for response...");
-            latch.await();
-        }
-        catch (Exception e) {
-            logger.error("Error: " + e.getMessage());
-        }
-        finally {
-            channel.shutdown().awaitTermination(10, TimeUnit.SECONDS);
+            switch (action) {
+                case "carePatient" -> {
+                    final String room = argMap.get(ClientArgs.ROOM.getValue());
+                    EmergencyCareRequest request = EmergencyCareRequest.newBuilder()
+                            .setRoom(Integer.parseInt(room))
+                            .build();
+                    EmergencyCareResponse response = ClientCallback.executeHandling(() -> blockingStub.startEmergencyCare(request));
+                    if (Objects.nonNull(response)) {
+                        System.out.println(String.format("Patient %s (%d) and Doctor %s (%d) are now in Room #$d",
+                                response.getPatientName(), response.getPatientLevel(), response.getDoctorName(), response.getDoctorLevel(), response.getRoom()));
+                    }
+                }
+                case "careAllPatient" -> {
+                    Empty request = Empty.newBuilder().build();
+                    /*EmergencyCareResponse response = ClientCallback.executeHandling(() -> blockingStub.startEmergencyCare(request));
+                    if (Objects.nonNull(response)) {
+                        System.out.println(String.format("Patient %s (%d) and Doctor %s (%d) are now in Room #$d",
+                                response.getPatientName(), response.getPatientLevel(), response.getDoctorName(), response.getDoctorLevel(), response.getRoom()));
+                    }*/
+                }
+                case "dischargePatient" -> {
+                    final String room = argMap.get(ClientArgs.ROOM.getValue());
+                    final String doctorName = argMap.get(ClientArgs.DOCTOR.getValue());
+                    final String patientName = argMap.get(ClientArgs.PATIENT.getValue());
+                    EmergencyCareRequest request = EmergencyCareRequest.newBuilder()
+                            .setRoom(Integer.parseInt(room))
+                            .setDoctorName(doctorName)
+                            .setPatientName(patientName)
+                            .build();
+                    EmergencyCareResponse response = ClientCallback.executeHandling(() -> blockingStub.endEmergencyCare(request));
+                    if(Objects.nonNull(response)) {
+                        System.out.println(String.format("Patient %d (%d) has been discharged from Doctor %s (%d) and the Room #$d is now %d",
+                                response.getPatientName(), response.getPatientLevel(), response.getDoctorName(), response.getDoctorLevel(), response.getRoom(), response.getRoomStatus()));
+                    }
+                }
+            }
+        } catch (StatusRuntimeException e) {
+            logger.error("gRPC failed: {}", e.getStatus());
+        } catch (Exception e) {
+            logger.error("Unexpected error: ", e);
+        } finally {
+            channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
         }
     }
+
 }
 
