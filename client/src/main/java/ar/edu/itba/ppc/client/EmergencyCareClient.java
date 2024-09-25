b65ebfe4245075fb03bfb058d2b04627cc2575f6
dlimachi <dlimachi@itba.edu.ar>
2024-09-24 22:03:52 -0300

    Added actions to clients

--- client/src/main/java/ar/edu/itba/ppc/client/EmergencyCareClient.java
+++ client/src/main/java/ar/edu/itba/ppc/client/EmergencyCareClient.java
@@ -1,37 +1,76 @@
 package ar.edu.itba.ppc.client;
 
-import ar.edu.itba.tp1g5.*;
+import ar.edu.itba.ppc.client.utilsConsole.ClientArgs;
+import ar.edu.itba.tp1g5.EmergencyCareRequest;
+import ar.edu.itba.tp1g5.EmergencyCareResponse;
+import ar.edu.itba.tp1g5.EmergencyCareServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+
+import static ar.edu.itba.ppc.client.utilsConsole.ClientUtils.parseArgs;
 
 public class EmergencyCareClient {
     private static Logger logger = LoggerFactory.getLogger(EmergencyAdminClient.class);
+    private static CountDownLatch latch;
 
     public static void main(String[] args) throws InterruptedException {
         logger.info("tp1-g5 Client Starting ...");
         logger.info("grpc-com-patterns Client Starting ...");
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50052)
+        Map<String,String> argMap = parseArgs(args);
+        final String serverAddress = argMap.get(ClientArgs.SERVER_ADDRESS.getValue());
+        final String action = argMap.get(ClientArgs.ACTION.getValue());
+
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(serverAddress)
                 .usePlaintext()
                 .build();
 
-        try {
-            EmergencyCareServiceGrpc.EmergencyCareServiceBlockingStub blockingStub =
-                    EmergencyCareServiceGrpc.newBlockingStub(channel);
+        EmergencyCareServiceGrpc.EmergencyCareServiceBlockingStub blockingStub =
+                EmergencyCareServiceGrpc.newBlockingStub(channel);
 
-            EmergencyCareRequest request = EmergencyCareRequest.newBuilder()
+        switch (action) {
+            case "carePatient" -> {
+                final String room = argMap.get(ClientArgs.ROOM.getValue());
+                latch = new CountDownLatch(1);
+                EmergencyCareRequest request = EmergencyCareRequest.newBuilder()
+                        .setRoom(Integer.parseInt(room))
+                        .build();
+                EmergencyCareResponse response = blockingStub.startEmergencyCare(request);
+                System.out.println(String.format("Patient %d and Doctor %s are now in Room #$d",
+                        response.getPatientName(), response.getDoctorName(), response.getRoom()));
+            }
+            case "dischargePatient" -> {
+                final String room = argMap.get(ClientArgs.ROOM.getValue());
+                final String doctorName = argMap.get(ClientArgs.DOCTOR.getValue());
+                final String patientName = argMap.get(ClientArgs.PATIENT.getValue());
+                latch = new CountDownLatch(1);
+                EmergencyCareRequest request = EmergencyCareRequest.newBuilder()
+                        .setRoom(Integer.parseInt(room))
+                        .setDoctorName(doctorName)
+                        .setPatientName(patientName)
+                        .build();
+                EmergencyCareResponse response = blockingStub.endEmergencyCare(request);
+                System.out.println(String.format("Patient %d has been discharged from Doctor %s and the Room #$d is now Free",
+                        response.getPatientName(), response.getDoctorName(), response.getRoom()));
+            }
+        }
+
+        try {
+            /*EmergencyCareRequest requested = EmergencyCareRequest.newBuilder()
                     .setRoom(1)
                     .build();
 
-            EmergencyCareResponse reply = blockingStub.startEmergencyCare(request);
+            EmergencyCareResponse reply = blockingStub.startEmergencyCare(requested);
             System.out.println(String.format("Start emergency care in %s with %s", reply.getRoom(), reply.getDoctorName()));
 
-
-
-
+             */
+            logger.info("Waiting for response...");
+            latch.await();
         }
         catch (Exception e) {
             logger.error("Error: " + e.getMessage());
