f364770176c312eb9ae716d00c1f470e832978ca
dlimachi <dlimachi@itba.edu.ar>
2024-09-25 12:56:58 -0300

    remove unused classes

--- server/src/main/java/ar/edu/itba/ppc/server/dto/Doctor.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.itba.ppc.server.dto;
-
-public record Doctor (
-        String doctorName,
-        Integer level,
-        String availability
-) {
-}
--- server/src/main/java/ar/edu/itba/ppc/server/exceptions/DoctorNotAssignedToRoomException.java
+++
@@ -1,6 +0,0 @@
-package ar.edu.itba.ppc.server.exceptions;
-
-public class DoctorNotAssignedToRoomException extends RuntimeException {
-    public DoctorNotAssignedToRoomException(String doctorName, Integer roomId) {
-    }
-}
